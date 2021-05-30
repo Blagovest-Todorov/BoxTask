6e9cebe6ab0fd5e92636b5a8e068d4b9291485e1
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-05-30 11:33:20 +0300

    add Program.cs , StartUp.cs

---
+++ Box.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxTask
+{
+    class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                this.ThrowIfInvalidSide(value, nameof(this.Length));
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                this.ThrowIfInvalidSide(value, nameof(this.Width));
+
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                this.ThrowIfInvalidSide(value, nameof(this.Height));
+                this.height = value;
+            }
+        }
+
+        public double CalcVolume()
+        {
+            return this.Length * this.Width * this.Height;
+        }
+
+        public double CalcLateralSurfaceArea() 
+        {
+            return 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+        }
+
+        public double CalcSurfaceArea() 
+        {
+            return 2 * this.Length * this.Height + 2 * this.Width * this.Height + 
+                2 * this.Length * this.Width;
+        }
+
+        private void ThrowIfInvalidSide(double value, string propertyName)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{propertyName} cannot be zero or negative.");
+            }
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BoxTask
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+                Console.WriteLine($"Surface Area - {box.CalcSurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalcLateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.CalcVolume():F2}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }            
+        }
+    }
+}
